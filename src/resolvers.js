0d7b37546f28de4cc1ab607177813531119f9d8e
Nwani Victory <vickywane@gmail.com>
2020-01-16 18:03:56 +0100

    storage buckets

--- src/resolvers.js
+++ src/resolvers.js
@@ -1,10 +1,13 @@
-require("dotenv").config(); //dotenv should be topmost so it loads all  env data
-import Axios from "axios";
-import bcrypt from "bcryptjs";
-import jwt from "jsonwebtoken";
-import { Storage } from "@google-cloud/storage";
-import passport from "passport";
-var GoogleStrategy = require("passport-google-oauth").OAuth2Strategy;
+require('dotenv').config(); //dotenv should be topmost so it loads all  env data
+import Axios from 'axios';
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import { Storage } from '@google-cloud/storage';
+import passport from 'passport';
+import Chance from 'chance';
+
+const path = require('path');
+var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 // import GoogleStrategy from "passport-google-oauth";
 
 passport.use(
@@ -12,7 +15,7 @@ passport.use(
     {
       clientID: process.env.CLIENT_ID,
       clientSecret: process.env.CLIENT_SECRET,
-      callbackURL: "http://www.example.com/auth/google/callback"
+      callbackURL: 'http://www.example.com/auth/google/callback',
     },
     function(accessToken, refreshToken, profile, done) {
       User.findOrCreate({ googleId: profile.id }, function(err, user) {
@@ -21,6 +24,11 @@ passport.use(
     }
   )
 );
+
+const storage = new Storage({
+  keyFilename: path.join(__dirname, '../Remotify-service-key.json'),
+  projectId: 'remotify-250023',
+});
 
 const resolver = {
   Query: {
@@ -31,9 +39,9 @@ const resolver = {
       const id = ctx.where.id;
       return prisma.db.query.event({
         where: {
-          email
+          email,
         },
-        info
+        info,
       });
     },
 
@@ -41,11 +49,11 @@ const resolver = {
       const id = ctx.where.id;
       return prisma.db.query.user({
         where: {
-          id
+          id,
         },
-        info
+        info,
       });
-    }
+    },
   },
 
   Mutation: {
@@ -53,15 +61,29 @@ const resolver = {
     createEvent: async (root, args, context, info) => {
       const hashedPassword = await bcrypt.hash(args.password, 10);
 
+      // generate random names for d buckets
+      const chance = new Chance();
+      const random = chance.string();
+      const Random = random.toLowerCase();
+      // ===========================================>
+
       // cloud function here
       const Email = args.email;
       try {
-        Axios.post("http://localhost:8080/", {
-          email: Email
+        // for local testing == 'http://localhost:8080/'
+        const link = process.env.EMAIL_FUNCTION;
+        Axios.post(`${link}/?email=${args.email}`, {
+          email: Email,
         });
       } catch (error) {
         console.log(error);
       }
+
+      await storage.createBucket(Random);
+      console.log(`Bucket ${Random} created.`);
+
+      const [details] = await storage.bucket(Random).getMetadata();
+      // console.log([details.name, details.selfLink]);
 
       return await context.db.mutation.createEvent({
         data: {
@@ -74,12 +96,12 @@ const resolver = {
           duration: 11,
           organizer: args.organizer,
           website: args.website,
-          bucketLink: args.bucketLink,
+          bucketLink: details.selfLink,
           supportEmail: args.supportEmail,
           teams: args.teams,
           attendees: args.attendees,
-          password: hashedPassword
-        }
+          password: hashedPassword,
+        },
       });
     },
 
@@ -89,8 +111,8 @@ const resolver = {
       // cloud function here
       const Email = args.email;
       try {
-        Axios.post("http://localhost:8080/", {
-          email: Email
+        Axios.post('http://localhost:8080/', {
+          email: Email,
         });
       } catch (error) {
         console.log(error);
@@ -105,8 +127,8 @@ const resolver = {
           email: args.email,
           country: args.country,
           state: args.state,
-          password: hashedPassword
-        }
+          password: hashedPassword,
+        },
       });
     },
 
@@ -118,25 +140,25 @@ const resolver = {
       const email = where.email;
       const user = await ctx.db.query.staff({
         where: {
-          email: email
-        }
+          email: email,
+        },
       });
 
       if (!user) {
-        throw new Error("Invalid Login");
+        throw new Error('Invalid Login');
       }
       const passwordMatch = await bcrypt.compare(password, user.password);
 
       if (!passwordMatch) {
-        throw new Error("Invalid Login");
+        throw new Error('Invalid Login');
       }
       const token = jwt.sign(
         {
-          username: email
+          username: email,
         },
         process.env.APP_SECRET,
         {
-          expiresIn: "30d"
+          expiresIn: '30d',
         }
       );
       return { token, user };
@@ -144,7 +166,7 @@ const resolver = {
 
     createGoogleUser: async (_, { email, where }, ctx, info) => {
       try {
-        passport.authenticate("google", { scope: ["profile"] });
+        passport.authenticate('google', { scope: ['profile'] });
 
         console.log(passport);
       } catch (e) {
@@ -159,14 +181,14 @@ const resolver = {
     sendEmail: (root, args, context, info) => {
       const Email = args.email;
       try {
-        Axios.post("http://localhost:8080/", {
-          email: Email
+        Axios.post('http://localhost:8080/', {
+          email: Email,
         });
       } catch (error) {
         console.log(error);
       }
-    }
-  }
+    },
+  },
 };
 
 export { resolver };
